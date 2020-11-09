6c66f2f63c2716e864efdb1c80fbf26e8ab11a27
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-11-09 11:25:14 -0500

    created initial popup. starting to set up functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import {DialogTitle, Dialog, List, ListItem} from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const initilaGrid = [[{content: '1'},{content: '2'},{content: '3'}],[{content: '4'},{content: '5'},{content: '6'}],[{content: '7'},{content: '8'},{content: '9'}]]
+  const [open, setOpen] = useState(true)
+  const [grid, setGrid] = useState(initilaGrid)
+
+  const handleClose = () => {
+    setOpen(true)
+  }
+
+  const handleInputChange = (event) => {
+    setGrid({...grid, [event.target.name]: event.target.value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">Grid</DialogTitle>
+      <List className='popUp'>
+        {grid.map((e, i) => (
+           <ListItem text onChange={handleInputChange} key={i}>
+            {e.content}
+          </ListItem>
+        ))}
+      </List>
+    </Dialog>
     </div>
   );
 }
